05e7081c9980f08add09a27e3378bdb470e258f6
stefanoHTB <bocchipinticorp@gmail.com>
2024-12-21 21:48:50 -0500

    add commands

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
 # remove unused packages
 apt-get auto-remove && apt-get clean
 
+sudo apt remove --purge neovim
+
+sudo apt autoremove
+
 # use terminal
 systemctl set-default multi-user.target
 
@@ -11,3 +15,20 @@ systemctl set-default graphical.target
 # add contrib to repos
 sudo add-apt-repository contrib
 
+
+
+
+source /root/.bashrc
+
+find / -name iptables
+find / -name ethtool
+find / -name tc
+
+
+which iptables
+
+
+echo 'export PATH=$PATH:/usr/sbin' >> ~/.bashrc
+source ~/.bashrc
+
+export PATH=$PATH:/usr/sbin
--- graphical.sh
+++ graphical.sh
@@ -14,5 +14,25 @@ systemctl set-default graphical.target
 sudo systemctl start gdm3
 
 # xorg
+#
+#
 
+
+# debugging
+
+# 1 expected: x11 or wayland
+
+echo $XDG_SESSION_TYPE
+
+
+# 2 check if gdm3 is in x11
+
+cat /etc/X11/default-display-manager
+
+sudo nano /etc/X11/default-display-manager
+
+# paste this inside: default-display-manager
+/usr/sbin/gdm3
+
+reboot
 
